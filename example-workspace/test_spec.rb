51dcebd7ee7bfe34688d3b85d6546f3fecebddc5
Yacine <anykeyh@gmail.com>
2021-03-06 12:02:43 +0700

    cleanup README.md

--- example-workspace/test_spec.rb
+++ example-workspace/test_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "test-coverage" do
   it "cover code" do
     100.times { |x| this_is_covered(x) }
 
-    # 10.times { this_is_half_covered }
+    10.times { this_is_half_covered }
     this_wont_be_covered
   end
 end
